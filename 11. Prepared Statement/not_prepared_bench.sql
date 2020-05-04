7f98b19ddacbfb0ccde391df5cf9973902995da0
Timur00Kh <newt300@mail.ru>
2020-05-04 15:43:49 +0300

    Update not_prepared_bench.sql

--- 11. Prepared Statement/not_prepared_bench.sql
+++ 11. Prepared Statement/not_prepared_bench.sql
@@ -1,5 +1,5 @@
 begin;
-prepare my_ps (text) as insert into prepared_table (name)
+prepare my_ps (varchar) as insert into prepared_table (name)
                        values ($1);
 execute my_ps(1, 'some name');
 deallocate my_ps;
